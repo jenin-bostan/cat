348e48f4cd52d116143fa2772ad0161fb6281d15
jenin <jenin.bostan@gmail.com>
2021-08-07 17:44:19 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    new WOW().init();
+
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        autoplay: true,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    })
+});
